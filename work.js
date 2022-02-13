0ef02b65631c8670e8fdaf7054568a3b878085a6
HARISANKARSL <94702116+HARISANKARSL@users.noreply.github.com>
2022-02-13 12:45:12 +0530

    Add files via upload

---
+++ work.js
@@ -0,0 +1,10 @@
+function  handlesubmit(){
+    let emails=document .getElementById("email");
+    let emailvalue=emails.value;
+   
+    let pass=document .getElementById("pass");
+    let passval=pass.value;
+
+alert ("youe email"+ emailvalue)
+
+}
